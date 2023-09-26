9dd97b1f4382c8815235b6d48f96458b40109ffd
Leonardo Lamoia <leonardotcinf@gmail.com>
2023-09-26 15:00:28 -0300

    created form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, Button } from 'react-native';
 
 import { styles } from './styles';
 
 export function Home() {
+
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -13,12 +18,20 @@ export function Home() {
         Sexta, 25 de setembro de 2023.
       </Text>
 
+    <View style={styles.form}>
       <TextInput
         style={styles.input}
         placeholder="Nome do Participante"
         placeholderTextColor='#6B6B6B'
         keyboardType='numeric'
       />
+
+      <TouchableOpacity style ={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
+      </View>
     </View>
   );
 }
